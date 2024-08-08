13c60235665487a56ceb5865f14798cb390d8151
Angel Medina <angelmedina909@yahoo.com>
2024-08-08 13:26:51 -0700

    Finish weather app

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey = "248570a4fc51e8b4d085ba8cf5b10e56";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+
+}
+
+searchBtn.addEventListener('click', ()=> {
+    checkWeather(searchBox.value);
+});
+
+checkWeather();
